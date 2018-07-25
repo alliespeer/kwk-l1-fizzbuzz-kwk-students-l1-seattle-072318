20d8841de84de9208849b425e8ea14f5daf77ea1
alliespeer <alliespeer13@gmail.com>
2018-07-25 15:59:06 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(num)
+  if num % 3==0 && 5 == 0 
+    return "fizzbuzz"
+  elsif num % 3 == 0 
+  return "fizz" 
+
+  elsif num % 5 == 0 
+  return "buzz"
+else
+  return NIL 
+end
+end
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
