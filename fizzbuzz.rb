fe802cbad5ec1a88f94b436644a05b0596d5accd
alliespeer <alliespeer13@gmail.com>
2018-07-25 16:06:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,12 @@
-def fizzbuzz(num)
-  if num % 3==0 && 5 == 0 
-    return "fizzbuzz"
-  elsif num % 3 == 0 
-  return "fizz" 
-
-  elsif num % 5 == 0 
-  return "buzz"
+def fizzbuzz(int = 6)
+  if int % 3 == 0 && (int % 5 == 0) 
+  "fizzbuzz"
+  elsif (int % 3 == 0) 
+  "fizz" 
+elsif (int % 5 == 0)
+   "buzz"
 else
-  return NIL 
+  "NIL"
 end
 end
-puts fizzbuzz(3)
-puts fizzbuzz(5)
-puts fizzbuzz(15)
+puts fizzbuzz(int =3)
