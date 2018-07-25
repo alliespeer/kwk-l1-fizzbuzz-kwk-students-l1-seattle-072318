520fd9eda2ee67bc32346b2d3f07e4b4c51d7ae2
alliespeer <alliespeer13@gmail.com>
2018-07-25 16:08:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,4 +9,30 @@ else
   "NIL"
 end
 end
-puts fizzbuzz(int =3)
+puts fizzbuzz
+
+def fizzbuzz(int = 5)
+  if int % 3 == 0 && (int % 5 == 0) 
+  "fizzbuzz"
+  elsif (int % 3 == 0) 
+  "fizz" 
+elsif (int % 5 == 0)
+   "buzz"
+else
+  "NIL"
+end
+end
+puts fizzbuzz(int =5)
+
+def fizzbuzz(int = 15)
+  if int % 3 == 0 && (int % 5 == 0) 
+  "fizzbuzz"
+  elsif (int % 3 == 0) 
+  "fizz" 
+elsif (int % 5 == 0)
+   "buzz"
+else
+  "NIL"
+end
+end
+puts fizzbuzz(int =15)
