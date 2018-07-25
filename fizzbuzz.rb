5dd17807ff4bb16282c3625ec91f5a2b01ea2d1c
alliespeer <alliespeer13@gmail.com>
2018-07-25 16:12:28 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(int = 3)
+  if int % 3 == 0 && (int % 5 == 0) 
+  "FizzBuzz"
+  elsif (int % 3 == 0) 
+  "Fizz" 
+elsif (int % 5 == 0)
+   "Buzz"
+    
+    end
+end
+
+puts fizzbuzz(int = 3)
